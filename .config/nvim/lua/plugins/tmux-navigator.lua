d56460b43de5ee1c151ace7cd9d49fd66fcdedba
Matthias Yee <matthias_yee@brown.edu>
2025-03-03 19:37:11 -0500

    tmux

--- .config/nvim/lua/plugins/tmux-navigator.lua
+++ .config/nvim/lua/plugins/tmux-navigator.lua
@@ -1,18 +1,16 @@
 return {
 	"christoomey/vim-tmux-navigator",
-	cmd = {
-		"TmuxNavigateLeft",
-		"TmuxNavigateDown",
-		"TmuxNavigateUp",
-		"TmuxNavigateRight",
-		"TmuxNavigatePrevious",
-		"TmuxNavigatorProcessList",
-	},
-	keys = {
-		{ "<C-h>", "<cmd><C-u>TmuxNavigateLeft<CR>" },
-		{ "<C-j>", "<cmd><C-u>TmuxNavigateDown<CR>" },
-		{ "<C-k>", "<cmd><C-u>TmuxNavigateUp<CR>" },
-		{ "<C-l>", "<cmd><C-u>TmuxNavigateRight<CR>" },
-		-- { "<C-\\>", "<cmd><C-u>TmuxNavigatePrevious<CR>" },
-	},
+	-- 	cmd = {
+	-- 		"TmuxNavigateLeft",
+	-- 		"TmuxNavigateDown",
+	-- 		"TmuxNavigateUp",
+	-- 		"TmuxNavigateRight",
+	-- 		"TmuxNavigatorProcessList",
+	-- 	},
+	-- 	keys = {
+	-- 		{ "<C-h>", "<cmd><C-u>TmuxNavigateLeft<CR>" },
+	-- 		{ "<C-j>", "<cmd><C-u>TmuxNavigateDown<CR>" },
+	-- 		{ "<C-k>", "<cmd><C-u>TmuxNavigateUp<CR>" },
+	-- 		{ "<C-l>", "<cmd><C-u>TmuxNavigateRight<CR>" },
+	-- 	},
 }
